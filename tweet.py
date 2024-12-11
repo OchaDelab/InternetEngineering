d885e7b8d48437e6e7c4aaad4e0306d409ba7765
OchaDelab <ochadelab@gmail.com>
2024-12-11 14:16:45 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,11 +1,11 @@
 import tweepy
 
 # APIキーとトークンの設定
-consumer_key = 'EaMB5BW19VMQjwkeybH0S7fux'
-consumer_secret = 'w4XhG1XJ2kVQXKP2Jf8nfT2ghDjqCobIcd5Ikhq98Mqc11Blit'
-access_token = '137940050-k44NsV9445h7ySZhZXDs8aVxXSYtVlpS8YtdkFdd'
-access_token_secret = '4cDHjDqCMASIdkIF9CXO296i23AKxMPkEob98kIMpd5di'
-bearer_token='AAAAAAAAAAAAAAAAAAAAAJ%2BLxQEAAAAAtiSvhN6%2F3i3YGokjUDywWWzTmWM%3D2udZtBuPWlvFh9pXBzAp8iKCzgj27vMKYpgJtlwP4SXPLKTTPc'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+bearer_token=''
 # 認証とAPIオブジェクトの作成
 #auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 #api = tweepy.API(auth)
